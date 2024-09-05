e548848a4ecd9c39b2c32f8cf41fe5a896ed7a9d
salesluis <salesluis434@gmail.com>
2024-09-04 23:11:24 -0300

    calculo do penultimo digito do cpf

---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+Calculo do primeiro dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 10
+
+Ex.:  746.824.890-70 (746824890)
+   10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0
+   70  36 48 56 12 20 32 27 0
+
+Somar todos os resultados: 
+70+36+48+56+12+20+32+27+0 = 301
+Multiplicar o resultado anterior por 10
+301 * 10 = 3010
+Obter o resto da divisão da conta anterior por 11
+3010 % 11 = 7
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O primeiro dígito do CPF é 7
+"""
+
+cpf = '74682489070'
+nove_digitos = cpf[:9]
+contador = 10
+primeiro_dig = 0
+segundo_dig = 0
+
+for digito in nove_digitos:
+    primeiro_dig += int(digito) * contador
+    contador -= 1
+
+primeiro_dig = (primeiro_dig * 10) % 11
+if primeiro_dig <= 9:
+    primeiro_dig
+else:
+    primeiro_dig = 0
+
+
+
